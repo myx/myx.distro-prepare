fc84d5e8f70cd5f1efcb67baaae9cfc8c8e3e465
Alexander Kharichev <myx@myx.co.nz>
2018-06-27 00:19:03 +0300

    + --merge-sequence option

--- sh-scripts/list-cached-project-provides.sh
+++ sh-scripts/list-cached-project-provides.sh
@@ -7,7 +7,27 @@ if [ -z "$MMDAPP" ] ; then
 	[ -d "$MMDAPP/source" ] || ( echo "expecting 'source' directory." >&2 && exit 1 )
 fi
 
+ListCachedProjectProvidesMergeSequence(){
+	local PKG="$1"
+	if [ -z "$PKG" ] ; then
+		echo "ListCachedProjectProvides: 'PKG' argument is required!" >&2 ; exit 1
+	fi
+
+	(
+		. "$MMDAPP/source/myx/myx.distro-prepare/sh-scripts/list-cached-project-sequence.sh" ;
+		for PROJECT in $( ListCachedProjectSequence "$PKG" ) ; do
+			ListCachedProjectProvides "$PROJECT"
+		done	
+	) | uniq		
+}
+
 ListCachedProjectProvides(){
+	if [ "$1" = "--merge-sequence" ] ; then
+		shift
+		ListCachedProjectProvidesMergeSequence "$@"
+		return 0
+	fi
+
 	local PKG="$1"
 	if [ -z "$PKG" ] ; then
 		echo "ListCachedProjectProvides: 'PKG' argument is required!" >&2 ; exit 1
@@ -38,6 +58,10 @@ ListCachedProjectProvides(){
 
 case "$0" in
 	*/sh-scripts/list-cached-project-provides.sh) 
+		if [ -z "$1" ] || [ "$1" = "--help" ] ; then
+			echo "syntax: list-cached-project-provides.sh [--help/--merge-sequence] <project_name>" >&2
+			exit 1
+		fi
 		ListCachedProjectProvides "$@"
 	;;
 esac
