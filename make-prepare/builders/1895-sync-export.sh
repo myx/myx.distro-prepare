82ec68f0bf911b7c225d0744c7f8ae74a30fbe16
Alexander Kharichev <myx@myx.co.nz>
2018-06-26 16:40:04 +0300

    * printf

--- make-prepare/builders/1895-sync-export.sh
+++ make-prepare/builders/1895-sync-export.sh
@@ -27,14 +27,14 @@ SyncExportsFromCached(){
 	local EXPORT_DST="$MMDAPP/output/export/$DST"
 
 	if test -e "$DISTRO_DIR/$SRC" ; then
-		printf %s "sync-export: $PKG $SRC (dis) \n \t \t <= $DST\n" >&2
+		printf 'sync-export: %s %s (dis) \n \t \t <= %s\n' "$PKG" "$SRC" "$DST" >&2
 		mkdir -p "`dirname "$EXPORT_DST"`"
 		rsync -ai --delete "$DISTRO_DIR/$SRC" "$EXPORT_DST"
 		return 0
 	fi
 	
 	if test -e "$SOURCE_DIR/$SRC" ; then
-		printf %s "sync-export: $PKG $SRC (src) \n \t \t <= $DST\n" >&2
+		printf 'sync-export: %s %s (src) \n \t \t <= %s\n' "$PKG" "$SRC" "$DST" >&2
 		mkdir -p "`dirname "$EXPORT_DST"`"
 		rsync -ai --delete "$SOURCE_DIR/$SRC" "$EXPORT_DST"
 		return 0
